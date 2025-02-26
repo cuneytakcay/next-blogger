368a3831c51bfcb081d5308cba10534bd3c9453d
cuneytakcay <cuneytakcay@gmail.com>
2025-02-26 14:05:33 -0700

    Adding create new post route

--- backend/routes/postsRoute.js
+++ backend/routes/postsRoute.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   try {
     const posts = await Post.find();
 
-    res.json(posts);
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -21,7 +21,27 @@ router.get('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    res.json(post);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create a new post
+// POST /api/posts
+router.post('/', async (req, res) => {
+  const { title, content, author } = req.body;
+
+  const newPost = new Post({
+    title,
+    content,
+    author,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+
+    res.status(201).json(savedPost);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
