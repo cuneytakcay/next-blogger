076ef9cf7b742e40227ff70708dd0cb2b1e14d00
cuneytakcay <cuneytakcay@gmail.com>
2025-03-01 16:00:24 -0700

    Adding delete and update user requests

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import isAuthenticated from '../middlewares/authenticateUser.js';
+import User from '../models/User.js';
 
 const router = express.Router();
 
@@ -20,6 +21,41 @@ router.get('/', isAuthenticated, (req, res) => {
       posts,
     },
   });
+});
+
+// Update user profile
+// PATCH /api/profile
+router.patch('/', isAuthenticated, async (req, res) => {
+  const { _id } = req.user;
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      message: 'User profile updated',
+      user,
+    });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
+
+// Delete user profile
+// DELETE /api/profile
+router.delete('/', isAuthenticated, async (req, res) => {
+  const { _id } = req.user;
+
+  try {
+    await User.findByIdAndDelete(_id);
+
+    res.status(200).json({
+      message: 'User account deleted',
+    });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 export default router;
