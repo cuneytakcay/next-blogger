fb5662776edb11147f5b682977256d3619e415c9
cuneytakcay <cuneytakcay@gmail.com>
2025-03-04 21:24:45 -0700

    Addin login page

---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../store/userSlice';
+import styles from './auth.module.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:5000/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const userData = await response.json();
+
+    if (response.ok) {
+      dispatch(setUser(userData));
+    } else {
+      console.error('Login failed:', userData.message);
+    }
+  };
+
+  return (
+    <div className={styles['form-container']}>
+      <h1 className={styles.title}>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles['input-container']}>
+          <input
+            id='email'
+            className={styles.input + ' ' + (email.length && styles.filled)}
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className={styles.label} htmlFor='email'>
+            Email
+          </label>
+        </div>
+        <div className={styles['input-container']}>
+          <input
+            id='password'
+            className={styles.input + ' ' + (password.length && styles.filled)}
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label className={styles.label} htmlFor='password'>
+            Password
+          </label>
+        </div>
+        <button className={styles.button} type='submit'>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
